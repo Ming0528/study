55ed3b6d67cc6190022b479b1b27296492b9dc27
liu.ming320 <liu.ming320@iwhalecloud.com>
2020-07-30 21:38:41 +0800

    fetch leetcode

---
+++ code/study-leetcode/src/main/java/com/study/leetcode/longestSubstring.java
@@ -0,0 +1,10 @@
+package com.study.leetcode;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * @author liu.ming320<br>
+ * @CreatedDate 2020/7/30 21:37 <br>
+ * @package com.study.leetcode <br>
+ */
+public class longestSubstring {
+}
