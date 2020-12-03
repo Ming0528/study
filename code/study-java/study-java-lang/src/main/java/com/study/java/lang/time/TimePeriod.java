a95e2b210328cc7981f660f22a23c44e05a7104d
liu.ming320 <liu.ming320@iwhalecloud.com>
2020-12-03 09:36:02 +0800

    feat

---
+++ code/study-java/study-java-lang/src/main/java/com/study/java/lang/time/TimePeriod.java
@@ -0,0 +1,40 @@
+package com.study.java.lang.time;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
+
+public class TimePeriod {
+    @Test
+    public void test() {
+        Date endDate = new Date();
+        Date startDate = DateUtils.addDays(endDate, -32);
+
+
+        List<Date> dateList = Stream.iterate(startDate, date -> DateUtils.addDays(date, 1)).limit(5).collect(Collectors.toList());
+        System.out.println("startDate is" + startDate);
+        System.out.println("endDate is" + endDate);
+//            .sorted(Comparator.comparing(EventUsageCQueryReader::getStartTime).reversed())
+
+        List<String> sortDate = new ArrayList<>();
+        sortDate.add("2020-01-02");
+        sortDate.add("2020-01-03");
+        sortDate.add("2029-01-03");
+        sortDate.add("2019-01-03");
+        sortDate.add("2019-02-03");
+        List<String> sortDated = sortDate.stream().sorted().collect(Collectors.toList());
+        System.out.println("sortDate is" + sortDate);
+        System.out.println("sortDated is" + sortDated);
+
+    }
+
+//    public long diffDays(Date from, Date to) {
+//        return DateUtil.differDateInDays(from, new Date(to.getTime() - 1), DateConstants.MINUTE);
+//    }
+}
